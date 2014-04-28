5e1dcfa48de940fbd54c59fbbf8e6f1dd18c2bad
Jeff Rabinowitz <rabinowitzjeffc@gmail.com>
2014-04-27 21:23:38 -0400

    finished server! kinda...

--- bin/steamy_server.py
+++ bin/steamy_server.py
@@ -5,6 +5,7 @@ from collections import deque
 from select import select
 from socket import *
 from struct import pack, unpack, calcsize
+from subprocess import Popen, PIPE
 
 def server_respond(server, gamerooms):
     """Accept on server and read handshake from accepted connection"""
@@ -12,7 +13,7 @@ def server_respond(server, gamerooms):
     # Receive 32 bytes from server
     handshake_buffer = conn.recv(32)
     # unpack two unsigned integers and a null-padded string
-    steamy_vers,game_vers,game_name = unpack("!IIs",handshake_buffer)
+    steamy_vers,game_vers,game_name = unpack("!II24s",handshake_buffer)
     game_name = game_name.partition(b'\0')[0] # unpad NULLs from string
 
     # For a valid game name, add player to deque and return
@@ -28,18 +29,47 @@ def server_respond(server, gamerooms):
 
 
 def check_gamerooms(gamerooms):
-    """Given a gameroom dict of deques, if a deque is full, start the given game
-    and empty then empty the deque."""
-    for game, room in gamerooms:
-        if count(room) == room.maxlen:
-            start_game(game, room)
+    """Given a gameroom dict of deques, if a deque is full, start the given
+    game, empty the deque, and return the process object."""
+
+    return [start_game(g,r) for g, r in gamerooms.items() if count(r)==r.maxlen]
 
 def start_game(game, room):
     """Given a full deque for a game room, start the server of the given game
     name and pipe into it the address info of the clients. The game server will
     respond in kind with its socket address information, which should be used to
     contact the individual game clients."""
-    pass
+
+
+    p = Popen([game+"_server"],stin=PIPE,stdout=PIPE)
+
+    # Read the server port as a uint16 and transmit to clients
+    port = p.stdout.read(2)
+
+    # Write the addresses of the clients to the new process
+    # client[1] is an address tuple: (hostname, port)
+    for client in room:
+        p.stdin.write(
+            inet_aton(client[1][0]) # convert quad-dot formatted address to binary
+            + pack("!H",client[1][1]) # pack port into a uint16 in network byte order
+            )
+        client.send(pack("!H",port))
+        client.close()
+
+    # clear deque
+    room.clear()
+
+    return p
+
+def wait_finished_servers(servers_list):
+    """Given a list of process objects for game servers, poll the servers for
+    their completion status and wait() and remove() finished ones."""
+    # for server in servers_list:
+    #     if server.poll() is not None:
+    #         server.wait()
+    #         servers_list.remove(server)
+    [s.wait() for s in servers_list if s.poll() is not None]
+    servers_list[:] = [s for s in servers_list if s.poll() is None]
 
 def main():
     # create a nonblocking server on 
@@ -55,11 +85,14 @@ def main():
     # create a dictionary of game room deques
     gamerooms = { "snake" : deque(maxlen=2) } 
 
+    # create a list of active game servers
+    activeservers = []
+
     # loop forever
     while inputs:
         readable, writable, exceptional = select(inputs, outputs, inputs, 0.1)
 
-        # if server has an incoming connection, accept on it
+        # if server has an incoming connection, accept on it and add to room
         if server in readable:
             client = server_respond(server, gamerooms)
             readable.remove(server)
@@ -69,7 +102,10 @@ def main():
             pass
 
         # check whether any games are ready to start
-        check_gamerooms(gamerooms)
+        activeservers.extend(check_gamerooms(gamerooms))
+
+        # wait on all terminated processes only if they are indeed finished
+        wait_finished_servers(activeservers)
 
 if __name__=="__main__":
     main()
