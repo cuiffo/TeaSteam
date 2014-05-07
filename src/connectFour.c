761ce3bb97d96b0ec0bfcf5636230513c7f51b2b
Eric Cuiffo <ecuiffo@gmail.com>
2014-05-07 13:49:12 -0400

    Changed connectFour.c to be multiplayer -- won't be sure if it works until finishing game server

--- src/connectFour.c
+++ src/connectFour.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ncurses.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <inttypes.h>
+
+typedef struct sockaddr SA;
 
 #ifndef BOARD_Y
 #define BOARD_Y 6
@@ -45,8 +52,14 @@ void fillHoles(WINDOW* window);
 void fillPlayer(WINDOW* window, int player);
 void fillCur(WINDOW* window);
 void draw(WINDOW* window);
-int playMove();
+int playMove(int col, int player);
 int checkWin();
+int open_clientfd(char* host, int port);
+int waitForReady();
+int submitMove(int fd, int col);
+int waitForOpponent(int fd, int *col);
+void dropDisk();
+int opponentTurn(int fd, WINDOW* window);
 
 
 /*
@@ -57,10 +70,15 @@ int boardWidth = 32;
 int boardHeight = 20;
 int curLocation = 3;
 int curPlayer = 1;
+int playerNum = 0;
 int turns = 0;
-int win = 0;
+int win_status = 0;
 
 int main() {
+
+  // Wait until server connection is established and opponent
+  // is confirmed.
+  int fd = waitForReady();
 
   // Initialize ncurses and some options.
   init();
@@ -68,7 +86,7 @@ int main() {
   // Initialize some custom colors.
   initColors();
 
-  // Create an 32x20 window
+  // Create a window (basically the border).
   boardHeight = SPOT_HEIGHT*SPOTS_Y + INNER_SPACING_Y*(SPOTS_Y - 1) + 2*OUTER_SPACING_Y;
   int height = BOARD_Y + boardHeight + 1 ;
   boardWidth = SPOT_WIDTH*SPOTS_X + INNER_SPACING_X*(SPOTS_X - 1) + 2*OUTER_SPACING_X;
@@ -80,35 +98,82 @@ int main() {
   // Draw window with initial settings.
   draw(window);
 
+  int gameInProgress = 1;
+  if (curPlayer != playerNum) {
+    int gameover = opponentTurn(fd, window);
+    if (gameover)
+      gameInProgress = 0;
+  }
+
   // Wait for a character input
-  while(1) {
+  while(gameInProgress) {
+
+    // Get the next key pressed.
     int c = wgetch(window);
+
+    // We only care about left, right and enter.
     if (c == 67) {
+
+      // If we pressed the right key, then move the current piece right
+      // unless it can't move anymore.
       curLocation++;
       curLocation = (curLocation > SPOTS_X-1) ? SPOTS_X-1 : curLocation;
       wclear(window);
       draw(window);
     } else if (c == 68) {
+
+      // If we pressed left, move left unless we can't go anymore.
       curLocation--;
       curLocation = (curLocation < 0) ? 0 : curLocation;
       wclear(window);
       draw(window);
     } else if (c == 10) {
-      if (playMove()) {
-        win = checkWin();
-        if (win) {
-          wclear(window);
-          draw(window);
-          break;
-        }
-        turns++;
-        curPlayer = (curPlayer == 1) ? 2 : 1;
-        curLocation = 3;
-        wclear(window);
-        draw(window);
-        if (turns == SPOTS_X*SPOTS_Y)
-          break;
-      }
+
+      // If we pressed enter, submit the move and see if it is successful.
+      int result = submitMove(fd, curLocation);
+
+      // Maybe we failed to hit a valid spot.
+      if (result == 4)
+        continue;
+
+      // But if we won with it, awesome.
+      else if (result == 5)
+        win_status = 1;
+
+      // Or maybe it ended up with a tie.
+      else if (result == 6)
+        win_status = 3;
+
+      // If we're here then our result was 3, meaning a successful move.
+      dropDisk(curLocation, window);
+
+      // If the win_status has changed, we're done.
+      if (win_status != 0)
+        break;
+
+      // Now wait for the other user to go.
+      curPlayer = (curPlayer == 1) ? 2 : 1;
+      int gameover = opponentTurn(fd, window);
+      if (gameover)
+        break;
+
+
+      // if (playMove() == 0)
+      //   continue;
+      // win = checkWin();
+      //  if (win) {
+      //    wclear(window);
+      //    draw(window);
+      //    break;
+      //  }
+      //turns++;
+      //curPlayer = (curPlayer == 1) ? 2 : 1;
+      //curLocation = 3;
+      //wclear(window);
+      //draw(window);
+      //if (turns == SPOTS_X*SPOTS_Y)
+      //  break;
+
     }
   }
 
@@ -128,10 +193,32 @@ void draw(WINDOW* window) {
   touchwin(window);
 
   // Text on top
+  if (win_status == 0) {
+    if (curPlayer == playerNum)
+      mvwprintw(window, 1, 10, "Your turn!", curPlayer);
+    else 
+      mvwprintw(window, 1, 6, "Waiting for opponent", curPlayer);
+  } else if (win_status == 1) {
+    wattrset(window, COLOR_PAIR(playerNum+2));
+    mvwprintw(window, 1, 10, "YOU WIN!", curPlayer);
+    wattrset(window, COLOR_PAIR(1));
+    mvwprintw(window, 2, 3, "Press any button to exit.");
+  } else if (win_status == 2) {
+    int opponent = (playerNum == 1) ? 2 : 1;
+    wattrset(window, COLOR_PAIR(opponent+2));
+    mvwprintw(window, 1, 9, "You lose :(", curPlayer);
+    wattrset(window, COLOR_PAIR(1));
+    mvwprintw(window, 2, 3, "Press any button to exit.");
+  } else if (win_status == 3) {
+    mvwprintw(window, 1, 9, "Yay, a tie!", curPlayer);
+    mvwprintw(window, 2, 3, "Press any button to exit.");
+  }
+
+  /*
   if (turns == SPOTS_X*SPOTS_Y) {
     mvwprintw(window, 1, 11, "It's a tie!");
     mvwprintw(window, 2, 3, "Press any button to exit.");
-  } else if (win == 0) {
+  } else if (win_status == 0) {
     mvwprintw(window, 1, 8, "Player %i's Turn", curPlayer);
   } else {
     wattrset(window, COLOR_PAIR(win+2));
@@ -139,6 +226,8 @@ void draw(WINDOW* window) {
     wattrset(window, COLOR_PAIR(1));
     mvwprintw(window, 2, 3, "Press any button to exit.");
   }
+
+  */
 
   // Draw the board
   wattrset(window, COLOR_PAIR(2));
@@ -150,7 +239,7 @@ void draw(WINDOW* window) {
   wattrset(window, COLOR_PAIR(4));
   fillPlayer(window, 2);
 
-  if (win == 0 && turns != SPOTS_X*SPOTS_Y) {
+  if (win_status == 0 && curPlayer == playerNum) {
     wattrset(window, COLOR_PAIR(curPlayer+2));
     fillCur(window);
   }
@@ -201,7 +290,7 @@ void fillBoard(WINDOW* window, int width) {
   memset(blank, ' ', width);
   blank[width] = '\0';
   int i;
-  for (i = BOARD_Y; i <= BOARD_Y+12; ++i) {
+  for (i = BOARD_Y; i < BOARD_Y+boardHeight; ++i) {
     mvwprintw(window, i, BOARD_X, "%s", blank);
   }
   free(blank);
@@ -212,9 +301,9 @@ void fillHoles(WINDOW* window) {
   memset(blank, ' ', SPOT_WIDTH);
   blank[SPOT_WIDTH] = '\0';
   int i, j;
-  for (i = BOARD_Y+1; i <= BOARD_Y+12; i+=2) {
+  for (i = BOARD_Y+1; i < BOARD_Y+boardHeight; i+=2) {
     for (j = BOARD_X+OUTER_SPACING_X; j < boardWidth-OUTER_SPACING_X; j+=SPOT_WIDTH+INNER_SPACING_X)
-      mvwprintw(window, i, j, blank);
+      mvwprintw(window, i, j, "%s", blank);
   }
   free(blank);
 }
@@ -226,7 +315,7 @@ void fillPlayer(WINDOW* window, int player) {
   int i;
   for (i = 0; i < 42; ++i) {
     if (board[i] == player) {
-      mvwprintw(window, (i/SPOTS_X)*2+BOARD_Y+1, (i%SPOTS_X)*(SPOT_WIDTH+INNER_SPACING_X)+BOARD_X+OUTER_SPACING_X, blank);
+      mvwprintw(window, (i/SPOTS_X)*2+BOARD_Y+1, (i%SPOTS_X)*(SPOT_WIDTH+INNER_SPACING_X)+BOARD_X+OUTER_SPACING_X, "%s", blank);
     }
   }
   free(blank);
@@ -236,16 +325,16 @@ void fillCur(WINDOW* window) {
   char *blank = malloc(SPOT_WIDTH+1);
   memset(blank, ' ', SPOT_WIDTH);
   blank[SPOT_WIDTH] = '\0';
-  mvwprintw(window, BOARD_Y-1, BOARD_X+OUTER_SPACING_X+(curLocation*(SPOT_WIDTH+INNER_SPACING_X)), blank);
+  mvwprintw(window, BOARD_Y-1, BOARD_X+OUTER_SPACING_X+(curLocation*(SPOT_WIDTH+INNER_SPACING_X)), "%s", blank);
   free(blank);
 }
 
-int playMove() {
+int playMove(int col, int player) {
   int i;
   for (i = 5; i >= 0; --i) {
-    int cur = i*7+curLocation;
+    int cur = i*7+col;
     if (board[cur] == 0) {
-      board[cur] = curPlayer;
+      board[cur] = player;
       return 1;
     }
   }
@@ -253,7 +342,7 @@ int playMove() {
 }
 
 int checkWin() {
-  
+
   // Check horizontals
   int row;
   for (row = 0; row < 6; ++row) {
@@ -283,7 +372,7 @@ int checkWin() {
       if (row2 == row+4)
         return board[row*7+piece];
     }
-    
+
     // Check negative diagonals
     for (piece = 0; piece < 4; ++piece) {
       int offset;
@@ -310,4 +399,142 @@ int checkWin() {
   }
 
   return 0;
+}
+
+
+int waitForReady() {
+
+  // Tell the server that we want to play a game.
+  int fd = open_clientfd("localhost", 5774);
+  if (fd < 0) {
+    perror(NULL);
+    exit(0);
+  }
+  char msg[32] = "\0\0\0\0\0\0\0\0connectFour\0\0\0\0\0\0\0\0\0\0\0\0\0";
+  write(fd, msg, 32);
+
+  // Wait for the response which includes our port we will talk to later on.
+  char res[4];
+  read(fd, res, 4);
+  uint16_t port = ntohs(res[2]);
+  printf("%c%c%" PRIu16 "\n", res[0], res[1], port);
+
+  // Create a new connection with the other port.
+  int gamefd = open_clientfd("localhost", (int)port);
+  if (gamefd < 0) {
+    perror(NULL);
+    exit(0);
+  }
+
+  // Wait for the server to tell us we're all ready.
+  int ready = 0;
+  while (!ready) {
+    if (read(gamefd, msg, 32) < 0) {
+      perror(NULL);
+      exit(0);
+    }
+    if (msg[0] == 1) {
+      playerNum = (int)msg[1];
+      break;
+    }
+  }
+
+  return gamefd;
+}
+
+
+/*
+ * When we have decided on a place we want to drop the token at,
+ * this will submit it to the server for approval and checking
+ * for win conditions.
+ */
+int submitMove(int fd, int col) {
+  
+  // Send the message to the server.
+  char msg[2];
+  msg[0] = 2;
+  msg[1] = (char)col;
+  write(fd, msg, 2);
+
+  // Recieve the response.
+  char res[10];
+  read(fd, res, 10);
+
+  return (int)res[0];
+}
+
+
+int waitForOpponent(int fd, int *col) {
+  
+  // Recieve the move
+  char res[10];
+  read(fd, res, 10);
+  if (res[0] == 3)
+    *col = (int)res[1];
+
+  return (int)res[0];
+}
+
+
+void dropDisk(int col, int player, WINDOW* window) {
+  playMove(col, player);
+  turns++;
+  curPlayer = (curPlayer == 1) ? 2 : 1;
+  curLocation = 3;
+  wclear(window);
+  draw(window);
+}
+
+int opponentTurn(int fd, WINDOW* window) {
+  int col = 0;
+  int result = waitForOpponent(fd, &col);
+
+  // They won...
+  if (result == 5)
+    win_status = 2;
+
+  // We tied!
+  else if (result == 6)
+    win_status = 3;
+
+  // Our turn! Drop the opponent disk and get outta here.
+  dropDisk(col, curPlayer, window);
+  curPlayer = (curPlayer == 1) ? 2 : 1;
+
+  // If the win_status has changed, we're done.
+  if (win_status != 0)
+    return 1;
+
+  return 0;
+}
+
+
+/*
+ * open_clientfd - open connection to server at <hostname, port> 
+ *   and return a socket descriptor ready for reading and writing.
+ *   Returns -1 and sets errno on Unix error. 
+ *   Returns -2 and sets h_errno on DNS (gethostbyname) error.
+ */
+/* $begin open_clientfd */
+int open_clientfd(char *hostname, int port) {
+  int clientfd;
+  struct hostent *hp;
+  struct sockaddr_in serveraddr;
+
+  if ((clientfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    return -1; /* check errno for cause of error */
+
+  /* Fill in the server's IP address and port */
+  if ((hp = gethostbyname(hostname)) == NULL)
+    return -2; /* check h_errno for cause of error */
+  bzero((char *) &serveraddr, sizeof(serveraddr));
+  serveraddr.sin_family = AF_INET;
+  bcopy((char *)hp->h_addr_list[0], 
+      (char *)&serveraddr.sin_addr.s_addr, hp->h_length);
+  serveraddr.sin_port = htons(port);
+
+  /* Establish a connection with the server */
+  if (connect(clientfd, (SA *) &serveraddr, sizeof(serveraddr)) < 0)
+    return -1;
+  return clientfd;
 }
